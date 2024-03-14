e79a5773aedf427d5e318655391eb095f2b781a6
Lê Minh Quân <quanlm1999@gmail.com>
2024-03-14 15:04:39 +0700

    Updat uninstall script vmoperator to delete CRD

--- vmoperator/uninstall.sh
+++ vmoperator/uninstall.sh
@@ -7,8 +7,12 @@ set -e
 ################################################################################
 STACK="vmoperator"
 NAMESPACE="vmoperator"
-
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+VMCLUSTER="${script_dir}/values.yml"
+kubectl delete -n "$NAMESPACE" -f "$VMCLUSTER"
 
 helm uninstall "$STACK" \
   --namespace "$NAMESPACE"
+
+
 kubectl delete --ignore-not-found=true namespace "$NAMESPACE"
